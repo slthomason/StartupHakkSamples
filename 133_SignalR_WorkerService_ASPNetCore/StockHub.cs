8e1cec6e90e2e35467f2756ae4c5a4300935060e
hamzastartuphakk <hamza@startuphakk.com>
2023-12-07 22:45:43 +0500

    SignalR and Worker Service in .NET Core

---
+++ 133_SignalR_WorkerService_ASPNetCore/StockHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication18
+{
+    public class StockHub : Hub
+    {
+        public async Task SendStockPrice(string stockName, decimal price)
+        {
+            await Clients.All.SendAsync("ReceiveStockPrice", stockName, price);
+        }
+    }
+}
---
+++ 133_SignalR_WorkerService_ASPNetCore/Worker.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication18
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IHubContext<StockHub> _stockHub;
+        private const string stockName = "Basic Stock Name";
+        private const decimal stockPrice = 100;
+
+        public Worker(ILogger<Worker> logger, IHubContext<StockHub> stockHub)
+        {
+            _logger = logger;
+            _stockHub = stockHub;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                //do some stock works
+
+                Random rnd = new Random();
+                decimal stockRaise = rnd.Next(1, 100);
+
+                await _stockHub.Clients.All.SendAsync("ReceiveStockPrice", stockName, stockPrice + stockRaise);
+                await Task.Delay(4000, stoppingToken);
+            }
+        }
+    }
+}
