8a25beb44c74841d2d81c960161f23ccd6abae61
hamzastartuphakk <hamza@startuphakk.com>
2023-12-12 01:56:11 +0500

    Global Error Handling in ASP.NET Core 8

---
+++ 135_Global_Error_Handling_ASPNetCore/Program.cs
@@ -0,0 +1,185 @@
+//Old Way: Exception Handling Midleware
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(
+                exception, "Exception occurred: {Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Server Error"
+            };
+
+            context.Response.StatusCode =
+                StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+
+
+
+
+//New Way: IExceptionHandler
+
+internal sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(
+            exception, "Exception occurred: {Message}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server error"
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
+internal sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        // Handle the exception, log errors.
+
+        return true;
+    }
+}
+
+//Configuring IExceptionHandler Implementations
+
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+app.UseExceptionHandler();
+
+//Chaining Exception Handlers
+
+internal sealed class BadRequestExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<BadRequestExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<BadRequestExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not BadRequestException badRequestException)
+        {
+            return false;
+        }
+
+        _logger.LogError(
+            badRequestException,
+            "Exception occurred: {Message}",
+            badRequestException.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = "Bad Request",
+            Detail = badRequestException.Message
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
+
+internal sealed class NotFoundExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<NotFoundExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<NotFoundExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not NotFoundException notFoundException)
+        {
+            return false;
+        }
+
+        _logger.LogError(
+            notFoundException,
+            "Exception occurred: {Message}",
+            notFoundException.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status404NotFound,
+            Title = "Not Found",
+            Detail = notFoundException.Message
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response
+            .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
+
+builder.Services.AddExceptionHandler<BadRequestExceptionHandler>();
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
