f44a89b5c409bab70b3a89abcc2869baa0638b36
hamzastartuphakk <hamza@startuphakk.com>
2023-12-14 02:40:26 +0500

    Distributed Caching with Redis and Response Caching in ASP.NET Core 8

---
+++ 137_Distributed_Caching_with_Redis_in_ASPNETCore/Country.cs
@@ -0,0 +1,31 @@
+﻿namespace WebApplication19
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Country> getData()
+        {
+            var country = new List<Country>
+        {
+            new()
+            {
+                Id = 1,
+                Name = "United States of America"
+            },
+            new()
+            {
+                Id=2,
+                Name="Canada"
+            },
+            new()
+            {
+                Id=3,
+                Name="Australia"
+            }
+        };
+            return country;
+        }
+    }
+}
---
+++ 137_Distributed_Caching_with_Redis_in_ASPNETCore/CountryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace WebApplication19
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, VaryByHeader = "Authorization")]
+    public class CountryController : ControllerBase
+    {
+        Country country;
+        IDistributedCache _distributedCache;
+        public CountryController(IDistributedCache distributedCache)
+        {
+            country = new Country();
+            _distributedCache = distributedCache;
+        }
+        [HttpGet("countries")]
+        public async Task<IActionResult> Countries()
+        {
+            string cachedCountry = await _distributedCache.GetStringAsync("country");
+            if (!string.IsNullOrEmpty(cachedCountry))
+            {
+                return Ok(new { cachedData = cachedCountry });
+            }
+            //add to redis cache
+            var countries = country.getData();
+            var serialize = JsonSerializer.Serialize(countries);
+            await _distributedCache.SetStringAsync("country", serialize);
+            return Ok(countries);
+        }
+        [HttpGet("cached/remove")]
+        public async Task<IActionResult> ClearCachedCountry()
+        {
+            await _distributedCache.RemoveAsync("country");
+            return Ok("Removed");
+        }
+    }
+}
