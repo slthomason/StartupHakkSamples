cc2dbfaeefbf7d36ac9d7e024a6f14198aa46981
hamzastartuphakk <hamza@startuphakk.com>
2024-03-15 22:03:23 +0500

    update

--- 184_PLINQ_Queries_in_DotNetCore/Program.cs
+++ 184_PLINQ_Queries_in_DotNetCore/Program.cs
@@ -1,1 +1,5 @@
+var parallelQuery = from item in data.AsParallel()
+                    where item.SomeCondition
+                    select item;
+
 data.AsParallel().ForAll(item => ProcessItem(item));
