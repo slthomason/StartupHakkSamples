5928b5c4d5f1c05786171e51fd23407a2e07ef01
hamzastartuphakk <hamza@startuphakk.com>
2024-05-30 14:10:53 -0500

    added solutions on arraylist vs list

---
+++ 197_ArrayList_vs_List/ArrayList_vs_List_Add_Elements/ArrayList_vs_List_Add_Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        const int iterations = 1000000;
+        var arrayList = new ArrayList();
+        var list = new List<int>();
+
+        var stopwatch = new Stopwatch();
+
+        // Adding elements to ArrayList
+        stopwatch.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            arrayList.Add(i);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"ArrayList: {stopwatch.ElapsedMilliseconds} ms");
+
+        // Adding elements to List<int>
+        stopwatch.Reset();
+        stopwatch.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            list.Add(i);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"List<int>: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
