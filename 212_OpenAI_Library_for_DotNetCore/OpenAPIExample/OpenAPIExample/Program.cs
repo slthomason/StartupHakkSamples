22d98d040472e593e59bf1ebcbe1fb787000809d
hamzastartuphakk <hamza@startuphakk.com>
2024-06-19 03:06:23 +0500

    added more video content

---
+++ 212_OpenAI_Library_for_DotNetCore/OpenAPIExample/OpenAPIExample/Program.cs
@@ -0,0 +1,9 @@
+﻿using OpenAI.Chat;
+
+ChatClient client = new(model: "gpt-4o", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+ChatMessage messages =
+    new UserChatMessage("What's the weather like today?");
+
+ChatCompletion completion = client.CompleteChat(messages);
+
+Console.WriteLine($"[ASSISTANT]: {completion}");
---
+++ 213_Implementing_gRPC_in_DotNetCore/aspnet-grpc-client/Program.cs
@@ -0,0 +1,29 @@
+﻿using aspnet_grpc; // Add reference to the grpc project
+using Grpc.Net.Client;
+
+// Configure gRPC channel
+using var channel = GrpcChannel.ForAddress("https://localhost:7047");
+var client = new ToDoService.ToDoServiceClient(channel);
+
+// Add new item ToDo
+var addReply = await client.AddToDoAsync(new AddToDoRequest { Description = "Learn gRPC" });
+Console.WriteLine(addReply.Message);
+
+// Get ToDo items
+var getReply = await client.GetToDosAsync(new GetToDosRequest());
+foreach (var item in getReply.Items)
+{
+    Console.WriteLine($"{item.Id}: {item.Description} (Completed: {item.IsCompleted})");
+}
+
+// Complete ToDo item
+var completeReply = await client.CompleteToDoAsync(new CompleteToDoRequest { Id = 1 });
+Console.WriteLine(completeReply.Message);
+
+// Get ToDo items again
+getReply = await client.GetToDosAsync(new GetToDosRequest());
+foreach (var item in getReply.Items)
+{
+    Console.WriteLine($"{item.Id}: {item.Description} (Completed: {item.IsCompleted})");
+}
+Console.ReadLine();
