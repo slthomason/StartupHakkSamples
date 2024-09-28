5a4896cef735c6defe5beb22b3da69ebdbcb4853
Talha Hafeez <talha@startuphakk.com>
2024-09-28 12:38:01 +0500

    added migrations for local db

---
+++ 235_Asp.netcore_With_docker/Code/AspnetCoreWithDocker/Migrations/20240928073506_initialize_db.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspnetCoreWithDocker.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialize_db : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
