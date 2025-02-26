850e99d78ed4ae0727e139d5b7b42d831ffde862
Nouman Manzoor <nouman.manzoor@innovallc.com>
2025-02-26 06:26:58 +0500

    NET_Wrapper_Ollama project

---
+++ 242_NET_Wrapper_Ollama project/NET_Wrapper_Ollama project/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.AI;
+
+IChatClient chatClient =
+    new OllamaChatClient(new Uri("http://localhost:11434/"), "phi3:mini");
+
+List<ChatMessage> chatHistory = new();
+
+while (true)
+{
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (var item in
+        chatClient.GetStreamingResponseAsync(chatHistory))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
